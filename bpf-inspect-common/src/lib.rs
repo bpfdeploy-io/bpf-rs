178b55dd61dba7502ff7b972a93e67313824851c
Milan <milan@mdaverde.com>
2022-04-01 11:21:20 -0400

    bpf-inspect-common: program type names and enum interator
    Signed-off-by: Milan <milan@mdaverde.com>

--- bpf-inspect-common/src/lib.rs
+++ bpf-inspect-common/src/lib.rs
@@ -1,9 +1,10 @@
+use num_enum::{TryFromPrimitive, IntoPrimitive};
 use std::time::Duration;
-use num_enum::TryFromPrimitive;
 
+/// Must abide by enum bpf_prog_type in kernel headers
 #[non_exhaustive]
 #[repr(u32)]
-#[derive(Debug, TryFromPrimitive)]
+#[derive(Debug, TryFromPrimitive, IntoPrimitive)]
 pub enum ProgramType {
     Unspec = 0,
     SocketFilter,
@@ -37,8 +38,67 @@ pub enum ProgramType {
     Lsm,
     SkLookup,
     Syscall,
-    Unknown = u32::MAX,
 }
+
+impl ProgramType {
+    pub fn name(&self) -> &'static str {
+        match *self {
+            ProgramType::Unspec => "unspec",
+            ProgramType::SocketFilter => "socket_filter",
+            ProgramType::Kprobe => "kprobe",
+            ProgramType::SchedCls => "sched_cls",
+            ProgramType::SchedAct => "sched_act",
+            ProgramType::Tracepoint => "tracepoint",
+            ProgramType::Xdp => "xdp",
+            ProgramType::PerfEvent => "perf_event",
+            ProgramType::CgroupSkb => "cgroup_skb",
+            ProgramType::CgroupSock => "cgroup_sock",
+            ProgramType::LwtIn => "lwt_in",
+            ProgramType::LwtOut => "lwt_out",
+            ProgramType::LwtXmit => "lwt_xmit",
+            ProgramType::SockOps => "sock_ops",
+            ProgramType::SkSkb => "sk_skb",
+            ProgramType::CgroupDevice => "cgroup_device",
+            ProgramType::SkMsg => "sk_msg",
+            ProgramType::RawTracepoint => "raw_tracepoint",
+            ProgramType::CgroupSockAddr => "cgroup_sock_addr",
+            ProgramType::LwtSeg6local => "lwt_seg6local",
+            ProgramType::LircMode2 => "lirc_mode2",
+            ProgramType::SkReuseport => "sk_reuseport",
+            ProgramType::FlowDissector => "flow_dissector",
+            ProgramType::CgroupSysctl => "cgroup_sysctl",
+            ProgramType::RawTracepointWritable => "raw_tracepoint_writable",
+            ProgramType::CgroupSockopt => "cgroup_sockopt",
+            ProgramType::Tracing => "tracing",
+            ProgramType::StructOps => "struct_ops",
+            ProgramType::Ext => "ext",
+            ProgramType::Lsm => "lsm",
+            ProgramType::SkLookup => "sk_lookup",
+            ProgramType::Syscall => "syscall",
+        }
+    }
+
+    pub fn iter() -> impl Iterator<Item = ProgramType> {
+        ProgramTypeIter(0)
+    }
+}
+
+struct ProgramTypeIter(u32);
+
+impl Iterator for ProgramTypeIter {
+    type Item = ProgramType;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let next = self.0;
+        if next > ProgramType::Syscall.into() {
+            None
+        } else {
+            self.0 = self.0 + 1;
+            ProgramType::try_from_primitive(next).ok()
+        }
+    }
+}
+
 
 
 #[derive(Debug)]
@@ -79,7 +139,6 @@ pub struct Program {
     pub run_time_ns: u64,
     pub run_cnt: u64,
 }
-
 
 #[cfg(test)]
 mod tests {
