222826f96a581441efb8201326b53f563ed4b6bb
Milan <milan@mdaverde.com>
2022-05-05 15:19:36 -0400

    bpf-feature: mod runtime
    Signed-off-by: Milan <milan@mdaverde.com>

--- bpf-feature/src/lib.rs
+++ bpf-feature/src/lib.rs
@@ -1,17 +1,14 @@
-use nix::sys::statfs::{statfs, PROC_SUPER_MAGIC};
-use std::path::Path;
 use thiserror::Error as ThisError;
 
-pub mod bpf;
-pub mod kernel_config;
-pub mod misc;
-
-#[cfg(feature = "serde")]
-use bpf_rs_macros::SerializeFromDisplay;
 #[cfg(feature = "serde")]
 use serde::Serialize;
 #[cfg(feature = "serde")]
 mod serde_ext;
+
+pub mod bpf;
+pub mod kernel_config;
+pub mod misc;
+pub mod runtime;
 
 #[derive(ThisError, Debug)]
 pub enum DetectError {
@@ -21,72 +18,11 @@ pub enum DetectError {
     CapSysAdmin,
 }
 
-#[derive(ThisError, Debug)]
-#[cfg_attr(feature = "serde", derive(SerializeFromDisplay))]
-pub enum RuntimeError {
-    #[error("procfs at /proc was not detected")]
-    ProcfsNonExistent,
-    #[error("std::num::ParseIntError: {0}")]
-    ParseIntError(#[from] std::num::ParseIntError),
-    #[error("std::io::Error: {0}")]
-    IO(#[from] std::io::Error),
-}
-
-type ProcfsResult = Result<usize, RuntimeError>;
-
-#[derive(Debug)]
-#[cfg_attr(feature = "serde", derive(Serialize))]
-pub struct Runtime {
-    #[cfg_attr(feature = "serde", serde(serialize_with = "serde_ext::flatten_result"))]
-    pub unprivileged_disabled: ProcfsResult,
-    #[cfg_attr(feature = "serde", serde(serialize_with = "serde_ext::flatten_result"))]
-    pub jit_enable: ProcfsResult,
-    #[cfg_attr(feature = "serde", serde(serialize_with = "serde_ext::flatten_result"))]
-    pub jit_harden: ProcfsResult,
-    #[cfg_attr(feature = "serde", serde(serialize_with = "serde_ext::flatten_result"))]
-    pub jit_kallsyms: ProcfsResult,
-    #[cfg_attr(feature = "serde", serde(serialize_with = "serde_ext::flatten_result"))]
-    pub jit_limit: ProcfsResult,
-}
-
-impl Runtime {
-    pub fn features() -> Result<Runtime, RuntimeError> {
-        Self::verify_procfs_exists()?;
-
-        Ok(Runtime {
-            unprivileged_disabled: Self::procfs_read(Path::new(
-                "/proc/sys/kernel/unprivileged_bpf_disabled",
-            )),
-            jit_enable: Self::procfs_read(Path::new("/proc/sys/net/core/bpf_jit_enable")),
-            jit_harden: Self::procfs_read(Path::new("/proc/sys/net/core/bpf_jit_harden")),
-            jit_kallsyms: Self::procfs_read(Path::new("/proc/sys/net/core/bpf_jit_kallsyms")),
-            jit_limit: Self::procfs_read(Path::new("/proc/sys/net/core/bpf_jit_limit")),
-        })
-    }
-
-    fn verify_procfs_exists() -> Result<(), RuntimeError> {
-        match statfs("/proc") {
-            Err(_) => Err(RuntimeError::ProcfsNonExistent),
-            Ok(stat) => {
-                if stat.filesystem_type() != PROC_SUPER_MAGIC {
-                    Err(RuntimeError::ProcfsNonExistent)
-                } else {
-                    Ok(())
-                }
-            }
-        }
-    }
-
-    fn procfs_read(path: &Path) -> ProcfsResult {
-        Ok(std::fs::read_to_string(path)?.trim().parse()?)
-    }
-}
-
 #[derive(Debug)]
 #[cfg_attr(feature = "serde", derive(Serialize))]
 pub struct Features {
     #[cfg_attr(feature = "serde", serde(serialize_with = "serde_ext::flatten_result"))]
-    pub runtime: Result<Runtime, RuntimeError>,
+    pub runtime: Result<runtime::Runtime, runtime::RuntimeError>,
     #[cfg_attr(feature = "serde", serde(serialize_with = "serde_ext::flatten_result"))]
     pub kernel_config: Result<kernel_config::KernelConfig, kernel_config::KernelConfigError>,
     #[cfg_attr(feature = "serde", serde(serialize_with = "serde_ext::flatten_result"))]
@@ -127,7 +63,7 @@ pub fn detect(opts: DetectOpts) -> Result<Features, DetectError> {
     }
 
     Ok(Features {
-        runtime: Runtime::features(),
+        runtime: runtime::features(),
         kernel_config: kernel_config::features(),
         bpf: bpf::features(bpf::BpfFeaturesOpts {
             full_helpers: opts.full_helpers,
---
+++ bpf-feature/src/runtime.rs
@@ -0,0 +1,71 @@
+use nix::sys::statfs::{statfs, PROC_SUPER_MAGIC};
+use std::path::Path;
+use thiserror::Error as ThisError;
+
+#[cfg(feature = "serde")]
+use crate::serde_ext;
+#[cfg(feature = "serde")]
+use bpf_rs_macros::SerializeFromDisplay;
+#[cfg(feature = "serde")]
+use serde::Serialize;
+
+#[derive(ThisError, Debug)]
+#[cfg_attr(feature = "serde", derive(SerializeFromDisplay))]
+pub enum RuntimeError {
+    #[error("procfs at /proc was not detected")]
+    ProcfsNonExistent,
+    #[error("std::num::ParseIntError: {0}")]
+    ParseIntError(#[from] std::num::ParseIntError),
+    #[error("std::io::Error: {0}")]
+    IO(#[from] std::io::Error),
+}
+
+type ProcfsResult = Result<usize, RuntimeError>;
+
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize))]
+pub struct Runtime {
+    #[cfg_attr(feature = "serde", serde(serialize_with = "serde_ext::flatten_result"))]
+    pub unprivileged_disabled: ProcfsResult,
+    #[cfg_attr(feature = "serde", serde(serialize_with = "serde_ext::flatten_result"))]
+    pub jit_enable: ProcfsResult,
+    #[cfg_attr(feature = "serde", serde(serialize_with = "serde_ext::flatten_result"))]
+    pub jit_harden: ProcfsResult,
+    #[cfg_attr(feature = "serde", serde(serialize_with = "serde_ext::flatten_result"))]
+    pub jit_kallsyms: ProcfsResult,
+    #[cfg_attr(feature = "serde", serde(serialize_with = "serde_ext::flatten_result"))]
+    pub jit_limit: ProcfsResult,
+}
+
+impl Runtime {
+    fn verify_procfs_exists() -> Result<(), RuntimeError> {
+        match statfs("/proc") {
+            Err(_) => Err(RuntimeError::ProcfsNonExistent),
+            Ok(stat) => {
+                if stat.filesystem_type() != PROC_SUPER_MAGIC {
+                    Err(RuntimeError::ProcfsNonExistent)
+                } else {
+                    Ok(())
+                }
+            }
+        }
+    }
+
+    fn procfs_read(path: &Path) -> ProcfsResult {
+        Ok(std::fs::read_to_string(path)?.trim().parse()?)
+    }
+}
+
+pub fn features() -> Result<Runtime, RuntimeError> {
+    Runtime::verify_procfs_exists()?;
+
+    Ok(Runtime {
+        unprivileged_disabled: Runtime::procfs_read(Path::new(
+            "/proc/sys/kernel/unprivileged_bpf_disabled",
+        )),
+        jit_enable: Runtime::procfs_read(Path::new("/proc/sys/net/core/bpf_jit_enable")),
+        jit_harden: Runtime::procfs_read(Path::new("/proc/sys/net/core/bpf_jit_harden")),
+        jit_kallsyms: Runtime::procfs_read(Path::new("/proc/sys/net/core/bpf_jit_kallsyms")),
+        jit_limit: Runtime::procfs_read(Path::new("/proc/sys/net/core/bpf_jit_limit")),
+    })
+}
