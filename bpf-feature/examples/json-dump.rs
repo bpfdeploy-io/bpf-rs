6f7ca15b7ce207d4c7c47d936ca38892a3f35a02
Milan <milan@mdaverde.com>
2022-05-08 10:09:26 -0400

    bpf-feature: allows for examples to compile without serde feature
    Signed-off-by: Milan <milan@mdaverde.com>

--- bpf-feature/examples/json-dump.rs
+++ bpf-feature/examples/json-dump.rs
@@ -1,7 +1,10 @@
-use bpf_feature::{detect, DetectOpts};
-
+#[cfg(feature = "serde")]
 fn main() {
+    use bpf_feature::{detect, DetectOpts};
     let features = detect(DetectOpts::default());
     let json_dump = serde_json::to_string_pretty(&features).unwrap();
     println!("{}", json_dump)
 }
+
+#[cfg(not(feature = "serde"))]
+fn main() {}
