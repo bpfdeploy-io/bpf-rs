3debc20ef78b83b0a25a6de181dd22cb1f2298f4
Milan <milan@mdaverde.com>
2022-11-08 12:54:36 -0500

    feat(bpf-rs): AttachFlags
    Types and serialization for the attachment flags that get set through
    the BPF_PROG_QUERY syscall from the kernel side. Currently only cgroup
    related ones are available.
    
    Signed-off-by: Milan <milan@mdaverde.com>

--- bpf-rs/src/attach_type.rs
+++ bpf-rs/src/attach_type.rs
@@ -3,6 +3,7 @@
 // strum generates a struct that by defaults propagates the same visibility
 // as the original enum. We don't want that and it isn't configurable so we
 // wrap it in a private module and re-export what we want public.
+// Is this still the case?
 mod private_hack {
     use num_enum::{IntoPrimitive, TryFromPrimitive};
     use std::ffi::CStr;
@@ -91,9 +92,51 @@ mod private_hack {
 
 pub use private_hack::AttachType;
 
+use bitflags::bitflags;
+
+#[cfg(feature = "serde")]
+use serde::Serialize;
+
+bitflags! {
+    /// These flags are currently only related to cgroup attachment types, but
+    /// that may change in the future: [Kernel src](https://github.com/torvalds/linux/blob/59f2f4b8a757412fce372f6d0767bdb55da127a8/include/uapi/linux/bpf.h#L1092)
+    pub struct AttachFlags: std::os::raw::c_uint {
+        const NONE = 0;
+        const ALLOW_OVERRIDE = 1 << 0;
+        const ALLOW_MULTI = 1 << 1;
+        const REPLACE = 1 << 2;
+    }
+}
+
+// TODO: This should eventually be replaced in bitflags v2?:
+// Link: https://github.com/bitflags/bitflags/issues/262#issuecomment-1271229962
+#[cfg(feature = "serde")]
+impl Serialize for AttachFlags {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeSeq;
+
+        let mut seq = serializer.serialize_seq(None)?;
+        if !self.is_empty() {
+            if self.contains(AttachFlags::ALLOW_OVERRIDE) {
+                seq.serialize_element("ALLOW_OVERRIDE")?;
+            }
+            if self.contains(AttachFlags::ALLOW_MULTI) {
+                seq.serialize_element("ALLOW_MULTI")?;
+            }
+            if self.contains(AttachFlags::REPLACE) {
+                seq.serialize_element("REPLACE")?;
+            }
+        }
+        seq.end()
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::attach_type::AttachType;
+    use super::AttachType;
 
     #[test]
     fn test_attach_types() {
@@ -103,5 +146,41 @@ mod tests {
         );
 
         AttachType::iter().for_each(|ty| assert!(!format!("{}", ty).is_empty()))
+    }
+
+    #[cfg(feature = "serde")]
+    #[test]
+    fn test_attach_flags_serialization() {
+        use super::AttachFlags;
+        use serde_test::{assert_ser_tokens, Token};
+
+        let flags = AttachFlags::ALLOW_OVERRIDE | AttachFlags::REPLACE;
+        assert_ser_tokens(
+            &flags,
+            &[
+                Token::Seq { len: None },
+                Token::String("ALLOW_OVERRIDE"),
+                Token::String("REPLACE"),
+                Token::SeqEnd,
+            ],
+        );
+
+        let flags = AttachFlags::NONE;
+        assert_ser_tokens(&flags, &[Token::Seq { len: None }, Token::SeqEnd]);
+
+        let flags = AttachFlags::empty();
+        assert_ser_tokens(&flags, &[Token::Seq { len: None }, Token::SeqEnd]);
+
+        let flags = AttachFlags::all();
+        assert_ser_tokens(
+            &flags,
+            &[
+                Token::Seq { len: None },
+                Token::String("ALLOW_OVERRIDE"),
+                Token::String("ALLOW_MULTI"),
+                Token::String("REPLACE"),
+                Token::SeqEnd,
+            ],
+        );
     }
 }
